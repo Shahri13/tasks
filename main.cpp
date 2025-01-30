40cec226e7d5dd9665e27492c28c9069251c7810
s.ulugbekova <s.ulugbekova@newuu.uz>
2025-01-30 15:00:42 +0500

    15

---
+++ main.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+using namespace std;
+int main() {
+  /*cout<<"problem 1"<<endl;
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    int sum = a+b+c+d+e;
+    cout << "sum is :"<< sum << endl;*/
+
+   /* cout<<"problem2"<<endl;
+    int id, year_of_study, study_fee;
+    float gpa;
+    cout<<"enter the id: "<<endl;
+    cin>>id;
+    cout<<"enter the gpa: "<<endl;
+    cin>>gpa;
+    cout<<"enter the year of study: "<<endl;
+    cin>>year_of_study;
+    cout<<"enter the study fee: "<<endl;
+    cin>>study_fee; */
+
+    /*cout<<"problem3"<<endl;
+    float celcius,fahrenheit;
+    cout<<"enter the temperature in degrees:"<<endl;
+    cin>>celcius;
+    fahrenheit=(celcius*9/5)+32;
+    cout<<fahrenheit<<endl;*/
+
+    /*cout<<"problem4"<<endl;
+    double cost_per_item, quantity, total_price;
+    cout<<"enter the cost of per item: "<<endl;
+    cin>>cost_per_item;
+    cout<<"enter the quantity: "<<endl;
+    cin>>quantity;
+    total_price=cost_per_item*quantity;
+    cout<<"total price: "<<total_price<<endl;*/
+
+    /*cout<<"problem5"<<endl;
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    float average=(a+b+c+d)/4;
+    cout<<"average: "<<average<<endl;*/
+
+    /*cout<<"problem6"<<endl;
+    int length, width;
+    cout<<"enter the length: "<<endl;
+    cin>>length;
+    cout<<"enter the width: "<<endl;
+    cin>>width;
+    int perimeter= (length+width)*2;
+    cout<<"perimeter: "<<perimeter<<endl;
+    int area= length*width;
+    cout<<"area: "<<area<<endl;*/
+
+   /* cout<<"problem7"<<endl;
+    int dividend,divisor,quotient,remainder;
+    cout<<"enter the dividend: "<<endl;
+    cin>>dividend;
+    cout<<"enter the divisor: "<<endl;
+    cin>>divisor;
+    quotient= dividend/divisor;
+    remainder=dividend%divisor;
+    cout<<"quotient= "<<quotient<<endl;
+    cout<<"remainder= "<<remainder<<endl; */
+
+    /*cout<<"problem8"<<endl;
+    int usd, rate,  uzs;
+    cout<<"enter the amoint in usd: "<<endl;
+    cin>>usd;
+    cout<<"enter the exchsnge rate: "<<endl;
+    cin>>rate;
+    uzs=usd*rate;
+    cout<<"amount in uzs: "<<uzs<<endl;*/
+
+    /*cout<<"problem9"<<endl;
+    int radius;
+    float area;
+    const float pi = 3.14;
+    cout<<"enter the radius: "<<endl;
+    cin>>radius;
+    area = pi*radius*radius;
+    cout<<"area: "<<area<<endl;*/
+
+    /*cout<<"problem10"<<endl;
+    int radius,height;
+    float volume;
+    const float pi = 3.14;
+    cout<<"enter the radius: "<<endl;
+    cin>>radius;
+    cout<<"enter the height: "<<endl;
+    cin>>height;
+    volume=pi*radius*radius*height;
+    cout<<"volume: "<<volume<<endl;*/
+
+    /*cout<<"problem11"<<endl;
+    int distance,time;
+    float velocity_in_kph,velocity_in_mph;
+    cout<<"enter the distance: "<<endl;
+    cin>>distance;
+    cout<<"enter the time: "<<endl;
+    cin>>time;
+    velocity_in_kph=distance/time;
+    cout<<"velocity_in_kph="<<velocity_in_kph<<endl;
+    velocity_in_mph=0.621*velocity_in_kph;
+    cout<<"velocity_in_mph="<<velocity_in_mph<<endl;*/
+
+   /* cout<<"problem12"<<endl;
+    int minutes,hours,rem_minutes;
+    cout<<"enter total minutes: "<<endl;
+    cin>>minutes;
+    hours= minutes/60;
+    rem_minutes=minutes%60;
+    cout<< "total time: "<< hours << " hours " << rem_minutes << " minutes " << endl; */
+
+   /* cout<<"problem13"<<endl;
+    int cost,fixed_cost,variable_cost,BEP;
+     cout<<"enter selling price per unit: "<<endl;
+     cin>>cost;
+     fixed_cost=10000;
+     cout<<"enter variable cost per unit; "<<endl;
+     cin>>variable_cost;
+     BEP=fixed_cost/(cost-variable_cost);
+     cout<<"BEP="<<BEP<<endl;   */
+
+ /*cout<<"problem14"<<endl;
+ int salary,tax_per,net_salary;
+ cout<<"enter the salary: "<<endl;
+ cin>>salary;
+ tax_per=12;
+ net_salary=(salary-(salary*tax_per)/100);
+ cout<<"net_salary: "<<net_salary<<endl;  */
+
+ /*cout<<"problem15"<<endl;
+ int or_p,disc_p,disc_a,final_p;
+ cout<<"enter the or_p: "<<endl;
+ cin>>or_p;
+ cout<<"enter the disc_p: "<<endl;
+ cin>>disc_p;
+ disc_a=or_p*disc_p/100;
+ final_p=or_p-disc_a;
+ cout<<"d_a= "<<disc_a<<endl;
+ cout<<"f_p= "<<final_p<<endl; */
+ 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
